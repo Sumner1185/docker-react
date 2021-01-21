e748adc7ddf6001c9bea6b2c96989dc902f23f38
Alastair Sumner <alastair-sumner@ecs-digital.co.uk>
2021-01-21 11:12:13 +0000

    Changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi Alastair
+          Wassup, I am the new app, changed on the feature branch
         </p>
         <a
           className="App-link"
